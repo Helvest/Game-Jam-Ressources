6f45cf5d2504f0bae59f89f76a027a51f1657cfe
Pedro Dominicé <b.w.f@outlook.com>
2017-05-05 15:32:46 +0200

    update Easing script

--- Assets/Scripts/Tools/Easing.cs
+++ Assets/Scripts/Tools/Easing.cs
@@ -6,7 +6,9 @@
 public static class Easing
 {
 	private const float PI = Mathf.PI;
-	private const float HalfPI = (PI / 2);
+	private const float HalfPI = Mathf.PI / 2;
+	private const float DoublePI = Mathf.PI * 2;
+	private const float HalfOne = .5f;
 
 	/// <summary>
 	/// Calculate the Ease from a pourcent
@@ -19,6 +21,7 @@ public static class Easing
 	{
 		switch(part)
 		{
+			default:
 			case EasingPart.NoEase:
 				return linearStep;
 			case EasingPart.EaseIn:
@@ -27,8 +30,8 @@ public static class Easing
 				return EaseOut(linearStep, type);
 			case EasingPart.EaseInOut:
 				return EaseInOut(linearStep, type);
-			default:
-				return linearStep;
+			case EasingPart.EaseOutIn:
+				return EaseOutIn(linearStep, type);
 		}
 	}
 
@@ -114,7 +117,8 @@ public static class Easing
 		switch(type)
 		{
 			case EasingType.Step:
-				return linearStep < .5f ? 0 : 1;
+				return Mathf.Round(linearStep);
+			default:
 			case EasingType.Linear:
 				return linearStep;
 			case EasingType.Sine:
@@ -129,8 +133,14 @@ public static class Easing
 				return Power.EaseIn(linearStep, 5);
 			case EasingType.Elastic:
 				return Elastic.EaseIn(linearStep);
-			default:
-				return linearStep;
+			case EasingType.Bounce:
+				return Bounce.EaseIn(linearStep);
+			case EasingType.Back:
+				return Back.EaseIn(linearStep);
+			case EasingType.Expo:
+				return Expo.EaseIn(linearStep);
+			case EasingType.Circ:
+				return Circ.EaseIn(linearStep);
 		}
 	}
 
@@ -144,7 +154,8 @@ public static class Easing
 		switch(type)
 		{
 			case EasingType.Step:
-				return linearStep < .5f ? 0 : 1;
+				return Mathf.Round(linearStep);
+			default:
 			case EasingType.Linear:
 				return linearStep;
 			case EasingType.Sine:
@@ -159,8 +170,14 @@ public static class Easing
 				return Power.EaseOut(linearStep, 5);
 			case EasingType.Elastic:
 				return Elastic.EaseOut(linearStep);
-			default:
-				return linearStep;
+			case EasingType.Bounce:
+				return Bounce.EaseOut(linearStep);
+			case EasingType.Back:
+				return Back.EaseOut(linearStep);
+			case EasingType.Expo:
+				return Expo.EaseOut(linearStep);
+			case EasingType.Circ:
+				return Circ.EaseOut(linearStep);
 		}
 
 	}
@@ -186,7 +203,8 @@ public static class Easing
 		switch(type)
 		{
 			case EasingType.Step:
-				return linearStep < .5f ? 0 : 1;
+				return Mathf.Round(linearStep);
+			default:
 			case EasingType.Linear:
 				return linearStep;
 			case EasingType.Sine:
@@ -201,60 +219,149 @@ public static class Easing
 				return Power.EaseInOut(linearStep, 5);
 			case EasingType.Elastic:
 				return Elastic.EaseInOut(linearStep);
+			case EasingType.Bounce:
+				return Bounce.EaseInOut(linearStep);
+			case EasingType.Back:
+				return Back.EaseInOut(linearStep);
+			case EasingType.Expo:
+				return Expo.EaseInOut(linearStep);
+			case EasingType.Circ:
+				return Circ.EaseInOut(linearStep);
+		}
+	}
+
+	/// <summary>
+	/// Calculate a Ease OutIn from a pourcent
+	/// </summary>
+	/// <param name="linearStep">Pourcent on the ease</param>
+	/// <param name="easeInType">Easing Type for the In</param>
+	/// <param name="easeOutType">Easing Type for the Out</param>
+	public static float EaseOutIn(float linearStep, EasingType easeInType, EasingType easeOutType)
+	{
+		return linearStep < 0.5 ? EaseOutIn(linearStep, easeInType) : EaseOutIn(linearStep, easeOutType);
+	}
+
+	/// <summary>
+	/// Calculate a Ease OutIn from a pourcent
+	/// </summary>
+	/// <param name="linearStep">Pourcent on the ease</param>
+	/// <param name="type">Easing Type</param>
+	public static float EaseOutIn(float linearStep, EasingType type)
+	{
+		switch(type)
+		{
+			case EasingType.Step:
+				return Mathf.Round(linearStep);
 			default:
+			case EasingType.Linear:
 				return linearStep;
+			case EasingType.Sine:
+				return Sine.EaseOutIn(linearStep);
+			case EasingType.Quadratic:
+				return Power.EaseOutIn(linearStep, 2);
+			case EasingType.Cubic:
+				return Power.EaseOutIn(linearStep, 3);
+			case EasingType.Quartic:
+				return Power.EaseOutIn(linearStep, 4);
+			case EasingType.Quintic:
+				return Power.EaseOutIn(linearStep, 5);
+			case EasingType.Elastic:
+				return Elastic.EaseOutIn(linearStep);
+			case EasingType.Bounce:
+				return Bounce.EaseOutIn(linearStep);
+			case EasingType.Back:
+				return Back.EaseOutIn(linearStep);
+			case EasingType.Expo:
+				return Expo.EaseOutIn(linearStep);
+			case EasingType.Circ:
+				return Circ.EaseOutIn(linearStep);
 		}
 	}
 
-	static class Sine
+	private static class Sine
 	{
-		public static float EaseIn(float s)
+		public static float EaseIn(float t)
 		{
-			return Mathf.Sin(s * HalfPI - HalfPI) + 1;
+			return Mathf.Sin(t * HalfPI - HalfPI) + 1;
 		}
-		public static float EaseOut(float s)
+
+		public static float EaseOut(float t)
 		{
-			return Mathf.Sin(s * HalfPI);
+			return Mathf.Sin(t * HalfPI);
 		}
-		public static float EaseInOut(float s)
+
+		public static float EaseInOut(float t)
 		{
-			return (Mathf.Sin(s * PI - HalfPI) + 1) / 2;
+			if(t == 0)
+				return 0;
+			if(t == HalfOne)
+				return HalfOne;
+			if(t == 1)
+				return 1;
+
+			return (Mathf.Sin(t * PI - HalfPI) + 1) / 2;
+		}
+
+		public static float EaseOutIn(float t)
+		{
+			if(t == 0)
+				return 0;
+			if(t == HalfOne)
+				return HalfOne;
+			if(t == 1)
+				return 1;
+
+			return t < HalfOne ?
+				EaseOut(t * 2) * HalfOne :
+				EaseIn((t * 2) - 1) * HalfOne + HalfOne;
 		}
 	}
 
-	static class Power
+	private static class Power
 	{
-		public static float EaseIn(float s, int power)
+		public static float EaseIn(float t, int power)
 		{
-			return Mathf.Pow(s, power);
+			return Mathf.Pow(t, power);
 		}
-		public static float EaseOut(float s, int power)
+
+		public static float EaseOut(float t, int power)
 		{
-			var sign = power % 2 == 0 ? -1 : 1;
-			return (sign * (Mathf.Pow(s - 1, power) + sign));
+			float sign = power % 2 == 0 ? -1 : 1;
+			return (sign * (Mathf.Pow(t - 1, power) + sign));
 		}
-		public static float EaseInOut(float s, int power)
+
+		public static float EaseInOut(float t, int power)
 		{
-			s *= 2;
-			if(s < 1)
-				return EaseIn(s, power) / 2;
-			var sign = power % 2 == 0 ? -1 : 1;
-			return (float)(sign / 2.0 * (Mathf.Pow(s - 2, power) + sign * 2));
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseIn(t * 2, power) * HalfOne:
+				EaseOut(t * 2 - 1, power) * HalfOne + HalfOne;
+		}
+
+		public static float EaseOutIn(float t, int power)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseOut(t * 2, power) * HalfOne :
+				EaseIn(t * 2 - 1, power) * HalfOne + HalfOne;
 		}
 	}
 
-	static class Elastic
+	private static class Elastic
 	{
-		private static float s, p;
+		private const float p = DoublePI /.3f;
+		private const float s = .3f / DoublePI * 1.57079633f;
 
 		public static float EaseIn(float t)
 		{
 			if(t == 0 || t == 1)
 				return t;
 
-			p = .3f;
-			s = p / (2 * PI) * Mathf.Asin(1);
-			return -(Mathf.Pow(2, 10 * (t -= 1)) * Mathf.Sin((t - s) * (2 * PI) / p));
+			return -(Mathf.Pow(2, 10 * (t -= 1)) * Mathf.Sin((t - s) * p));
 		}
 
 		public static float EaseOut(float t)
@@ -262,27 +369,175 @@ public static class Easing
 			if(t == 0 || t == 1)
 				return t;
 
-			p = .3f;
-			s = p / (2 * PI) * Mathf.Asin(1);
-			return Mathf.Pow(2, -10 * t) * Mathf.Sin((t - s) * (2 * PI) / p) + 1;
+			return Mathf.Pow(2, -10 * t) * Mathf.Sin((t - s) * p) + 1;
 		}
 
 		public static float EaseInOut(float t)
 		{
-			if(t == 0 || t == 1)
+			if(t == 0 || t == HalfOne || t == 1)
 				return t;
 
-			p = .3f;
-			s = p / (2 * PI) * Mathf.Asin(1);
+			return t < HalfOne ?
+				EaseIn(t * 2) * HalfOne :
+				EaseOut(t * 2 - 1) * HalfOne + HalfOne;
+		}
 
-			if(t < .5f)
-			{
-				t *= 2;
-				return -.5f * (Mathf.Pow(2, 10 * (t -= 1)) * Mathf.Sin((t - s) * (2 * PI) / p));
-			}
-			t -= 0.5f;
-			t *= 2;
-			return .5f * Mathf.Pow(2, -10 * t) * Mathf.Sin((t - s) * (2 * PI) / p) + 1;
+		public static float EaseOutIn(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseOut(t * 2) * HalfOne :
+				EaseIn(t * 2 - 1) * HalfOne + HalfOne;
+		}
+	}
+
+	private static class Bounce
+	{
+		private const float a = 7.5625f;
+		private const float b = 2.75f;
+
+		public static float EaseIn(float t)
+		{
+			return 1 - EaseOut(1 - t);
+		}
+
+		public static float EaseOut(float t)
+		{
+			if((t /= 1) < 1 / b)
+				return (a * t * t);
+			else if(t < 2 / b)
+				return (a * (t -= (1.5f / b)) * t + .75f);
+			else if(t < 2.5 / b)
+				return (a * (t -= 2.25f / b) * t + .9375f);
+			return (a * (t -= (2.625f / b)) * t + .984375f);
+		}
+
+		public static float EaseInOut(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				(1 - EaseOut(1 - t * 2)) * HalfOne :
+				EaseOut(t * 2 - 1) * HalfOne + HalfOne;
+		}
+
+		public static float EaseOutIn(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseOut(t * 2) * HalfOne :
+				(1 - EaseOut(t * 2 - 2)) * HalfOne + HalfOne;
+		}
+	}
+
+	private static class Back
+	{
+		private const float s = 1.70158f;
+		private const float v = s * 1.525f;
+
+		public static float EaseIn(float t)
+		{
+			return (t /= 1) * t * ((s + 1) * t - s);
+		}
+
+		public static float EaseOut(float t)
+		{
+			return ((t = t / 1 - 1) * t * ((s + 1) * t + s) + 1);
+		}
+
+		public static float EaseInOut(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseIn(t * 2) * HalfOne :
+				EaseOut(t * 2 - 1) * HalfOne + HalfOne;
+		}
+
+		public static float EaseOutIn(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseOut(t * 2) * HalfOne :
+				EaseIn(t * 2 - 1) * HalfOne + HalfOne;
+		}
+	}
+
+	private static class Expo
+	{
+		public static float EaseIn(float t)
+		{
+			if(t == 0 || t == 1)
+				return t;
+			return Mathf.Pow(2, 10 * (t / 1 - 1));
+		}
+
+		public static float EaseOut(float t)
+		{
+			if(t == 0 || t == 1)
+				return t;
+			return 1 - Mathf.Pow(2, -10 * t / 1);
+		}
+
+		public static float EaseInOut(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				Mathf.Pow(2, 10 * (t * 2 / 1 - 1)) * HalfOne :
+				(1 - Mathf.Pow(2, -10 * (t * 2 - 1) / 1)) * HalfOne + HalfOne;
+		}
+
+		public static float EaseOutIn(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				(1 - Mathf.Pow(2, -10 * t * 2 / 1)) * HalfOne :
+				Mathf.Pow(2, 10 * ((t * 2 - 1) / 1 - 1)) * HalfOne + HalfOne;
+		}
+	}
+
+	private static class Circ
+	{
+		public static float EaseIn(float t)
+		{
+			return -(Mathf.Sqrt(1 - (t /= 1) * t) - 1);
+		}
+
+		public static float EaseOut(float t)
+		{
+			return Mathf.Sqrt(1 - (t = t / 1 - 1) * t);
+		}
+
+		public static float EaseInOut(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseIn(t * 2) * HalfOne :
+				EaseOut(t * 2 - 1) * HalfOne + HalfOne;
+		}
+
+		public static float EaseOutIn(float t)
+		{
+			if(t == 0 || t == HalfOne || t == 1)
+				return t;
+
+			return t < HalfOne ?
+				EaseOut(t * 2) * HalfOne :
+				EaseIn(t * 2 - 1) * HalfOne + HalfOne;
 		}
 	}
 }
@@ -295,7 +550,8 @@ public enum EasingPart
 	NoEase,
 	EaseIn,
 	EaseOut,
-	EaseInOut
+	EaseInOut,
+	EaseOutIn
 }
 
 /// <summary>
@@ -310,5 +566,9 @@ public enum EasingType
 	Cubic,
 	Quartic,
 	Quintic,
-	Elastic
+	Elastic,
+	Bounce,
+	Back,
+	Expo,
+	Circ
 }
