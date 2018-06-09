08af196a55353f21aaef21df37ec8bdbfcebd0f2
Pedro Dominicé <b.w.f@outlook.com>
2018-06-09 20:40:43 +0200

    Up 2018 + Import Minions Art Shaders

---
+++ Assets/ompuco-Dithering/CustomDepthEffect.cs
@@ -0,0 +1,19 @@
+﻿//dithering & bit depth effects
+//by ompu co (Sam Blye) 2018
+//please visit https://www.patreon.com/ompuco
+
+using UnityEngine;
+[ExecuteInEditMode][ImageEffectAllowedInSceneView]
+public class CustomDepthEffect : MonoBehaviour
+{
+
+    public Material precisionMat;
+	[Range(1,256)]
+	public int colorDepth = 1;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {	
+		precisionMat.SetInt("cDepth",colorDepth);
+        Graphics.Blit(src, dest, precisionMat);
+    }
+}
---
+++ Assets/ompuco-Dithering/DitherEffect.cs
@@ -0,0 +1,18 @@
+﻿//dithering & bit depth effects
+//by ompu co (Sam Blye) 2018
+//please visit https://www.patreon.com/ompuco
+
+using UnityEngine;
+[ExecuteInEditMode][ImageEffectAllowedInSceneView]
+public class DitherEffect : MonoBehaviour
+{
+
+    public Material ditherMat;
+	public float ditherPower = 32.0f;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+		ditherMat.SetFloat("dPower", ditherPower);
+        Graphics.Blit(src, dest, ditherMat);
+    }
+}
